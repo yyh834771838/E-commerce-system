83c2831151d8a6ab068f60cd2fb0dcf39adf73d3
yyh834771838 <834771838@qq.com>
2020-06-25 10:12:43 +0800

    项目初始化

---
+++ src/main/java/com/economic/demo/EcnomicalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.economic.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcnomicalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcnomicalSystemApplication.class, args);
+    }
+
+}
