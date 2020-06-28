bbb9243af46f814079617909e7cd9283575c0226
U201817080 <457110764@qq.com>
2020-06-28 22:55:08 +0800

    初始化

---
+++ src/main/java/com/economic/demo/zyc/controller/CartController.java
@@ -0,0 +1,52 @@
+package com.economic.demo.zyc.controller;
+
+import com.economic.demo.zyc.service.CartService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class CartController {
+    @Resource
+    private CartService cartService;
+
+    @RequestMapping("/all")
+    public List<Map<String,Object>> findAll(){
+        List<Map<String,Object>> list=cartService.findAll();
+        return list;
+    }
+
+    @RequestMapping("/save")
+    public String save(@RequestParam Map<String,String> map){
+        String msg="添加失败";
+        boolean flag=cartService.doSave(map);
+        if(flag){
+            msg="添加成功";
+        }
+        return msg;
+    }
+
+    @RequestMapping("/delete")
+    public String delete(@RequestParam Map<String,String> map){
+        String msg="删除失败";
+        boolean flag=cartService.doDelete(map);
+        if(flag){
+            msg="删除成功";
+        }
+        return msg;
+    }
+
+    @RequestMapping("/edit")
+    public String update(@RequestParam Map<String,String> map){
+        String msg="修改失败";
+        boolean flag=cartService.doUpdate(map);
+        if(flag){
+            msg="修改成功";
+        }
+        return msg;
+    }
+}
