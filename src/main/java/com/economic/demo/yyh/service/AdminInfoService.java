aebb5f7fc327ac383dbc15b7c7fe02c629aac8e3
yyh834771838 <834771838@qq.com>
2020-06-28 17:01:12 +0800

    叶宇航6/28

---
+++ src/main/java/com/economic/demo/yyh/service/AdminInfoService.java
@@ -0,0 +1,42 @@
+package com.economic.demo.yyh.service;
+
+import com.economic.demo.yyh.mapper.AdminInfoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+@Service
+public class AdminInfoService {
+
+    @Resource
+    private AdminInfoMapper adminInfoMapper;
+
+    public Map<String,Object> checkLogin(Map<String,String> map){
+        Map<String,Object> info=null;
+        try{
+            info=adminInfoMapper.findByNameAndPwd(map);
+        }catch (Exception ex){
+
+        }
+
+        return info;
+    }
+
+    public String checkRegister(Map<String,String> map){
+        Map<String,Object> info=null;
+        try{
+            info=adminInfoMapper.findByName(map);
+        }catch (Exception ex){
+
+        }
+        if (map == null || map.size() == 0 || map.get("u_name").equals("") || map.get("u_pwd").equals(""))
+            return "请先填写信息";
+        if (info != null && info.size() != 0)
+            return "账号已存在";
+        if (!map.get("u_pwd").equals(map.get("u_pwd2")))
+            return "两次密码不一致";
+        adminInfoMapper.register(map);
+        return "注册成功";
+    }
+}
