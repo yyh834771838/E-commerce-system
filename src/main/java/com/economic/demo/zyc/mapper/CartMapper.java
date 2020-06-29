46629d6840206de92c6ea99d85143d4ecc1abb4f
U201817080 <457110764@qq.com>
2020-06-29 20:56:09 +0800

    增加了删除和修改

--- src/main/java/com/economic/demo/zyc/mapper/CartMapper.java
+++ src/main/java/com/economic/demo/zyc/mapper/CartMapper.java
@@ -7,20 +7,23 @@ import org.apache.ibatis.annotations.*;
 @Mapper
 public interface CartMapper {
 
-    @Select("select * from oderdetails")
+    @Select("SELECT \n" +
+            "a.class2_id,a.photo,a.name,b.id,b.pro_id,b.menu_id,b.number,b.price \n" +
+            "FROM product a \n" +
+            "INNER JOIN oderdetails b on a.pro_id=b.pro_id")
     public List<Map<String,Object>> findAll();
 
     @Select("select * from oderdetails where menu_id=#{n}")
     public Map<String,Object> findByMenuid(int id);
 
     @Insert("insert into oderdetails(pro_id,menu_id,number,price)" +
-            "value(#{pro_id},#{menu_id},#{number},#{price})")
+            "value(#{order.pro_id},#{order.menu_id},#{order.number},#{order.price})")
     public int save(@Param("order") Map<String,String> map);
 
     @Update("update oderdetails " +
-            " set number=#{num}" +
-            " where id=#{id}")
-    public int update(@Param("t") Map<String,String> tea);
+            " set number=#{order.number}" +
+            " where id=#{order.id}")
+    public int update(@Param("order") Map<String,String> tea);
 
     @Delete("delete from oderdetails where id=#{id}")
     public int delete(int id);
