ca2e4ffb12130fe56fc2657fd92760dd71466172
Godlowd <a614567188@163.com>
2020-07-02 13:28:39 +0800

    用户订单查询支持状态检测

--- src/main/resources/static/Script/getOrder.js
+++ src/main/resources/static/Script/getOrder.js
@@ -10,36 +10,71 @@ function setTableHeader(body){
     return body
 }
 var OrderInfo;
+var cur_user_id=-1;
+jQuery(function () {
+    jQuery("#top_page").load("top.html");
+    getStatus();
+    function getStatus()
+    {
+        var cookies=document.cookie.split(";");
+        for(var i=0;i<cookies.length;i++)
+        {
+            var cookie=cookies[i].trim();
+            if(cookie.indexOf("info")==0)
+            {
+                var m=cookie.substring("info".length+1);
+                m = m.substring(3);
+                m = m.substring(0,m.length-4);
+                jQuery.post("/findIdByName",{"user_name":m},function (rst) {
+                    cur_user_id = rst;
+                },"json");
+                return;
+                console.log("the current user id is" + cur_user_id)
+            }
+        }
+    }
+})
+
 jQuery(function () {
     //在单击全部订单的按钮执行事件
     jQuery("#allorder").click(function () {
-        jQuery.post("/getOrderInfo",{"user_id": 1}, function (NamePriceList) {
+        jQuery.post("/getOrderInfo",{"user_id": cur_user_id}, function (NamePriceList) {
             OrderInfo = NamePriceList
             console.log("the length of the total list is: " + NamePriceList.length)
             var body = ""
             body = setTableHeader(body)
-            for(var i = 0; i < NamePriceList.length; i++)
+            if (NamePriceList.length!= 0)
             {
-                var json=NamePriceList[i];
-                //开始添加
+                for(var i = 0; i < NamePriceList.length; i++)
+                {
+                    var json=NamePriceList[i];
+                    //开始添加
+                    body+="<tr>";
+                    //添加商品名字和图片信息
+                    body+="<td class='table_product_des'>";
+                    //添加商品图片信息
+                    body += "<img src='images/iphone.jpg' alt='图片一' class='product_img'>"
+                    body += json.name;
+                    body += "</td>";
+                    //添加商品单价信息
+                    body += "<td style='vertical-align: center; text-align: center; background-color: #FFFFFF'>" + json.pro_price + "</td>"
+                    //添加商品数量信息
+                    body += "<td style='vertical-align: center; text-align: center; background-color: #FFFFFF'>" + json.pro_number + "</td>"
+                    //添加商品操作信息
+                    body += "<td style='vertical-align: center; text-align: center;background-color: #FFFFFF'>" + "评价跳转" + "</td>"
+                    //添加商品状态
+                    body += "<td style='vertical-align: center; text-align: center; background-color: #FFFFFF'>" + json.status + "</td>"
+                    //添加下单时间
+                    body += "<td style='vertical-align: center; text-align: center; background-color: #FFFFFF'>" + json.time + "</td>"
+                    //添加商品
+                    body+="</tr>";
+                }
+            }
+            else {
                 body+="<tr>";
-                //添加商品名字和图片信息
-                body+="<td class='table_product_des'>";
-                //添加商品图片信息
-                body += "<img src='images/iphone.jpg' alt='图片一' class='product_img'>"
-                body += json.name;
-                body += "</td>";
-                //添加商品单价信息
-                body += "<td style='vertical-align: center; text-align: center; background-color: #FFFFFF'>" + json.pro_price + "</td>"
-                //添加商品数量信息
-                body += "<td style='vertical-align: center; text-align: center; background-color: #FFFFFF'>" + json.pro_number + "</td>"
-                //添加商品操作信息
-                body += "<td style='vertical-align: center; text-align: center;background-color: #FFFFFF'>" + "评价跳转" + "</td>"
-                //添加商品状态
-                body += "<td style='vertical-align: center; text-align: center; background-color: #FFFFFF'>" + json.status + "</td>"
-                //添加下单时间
-                body += "<td style='vertical-align: center; text-align: center; background-color: #FFFFFF'>" + json.time + "</td>"
-                //添加商品
+                body +="<td style='font-size: 200%'>";
+                body += "您尚未购买过商品"
+                body +="</td>";
                 body+="</tr>";
             }
             jQuery("#tbody").html(body)
